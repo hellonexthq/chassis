5383410fb2401aadf188b008e67470129a5e4b5d
Karthik Kamalakannan <karthik@hellonext.co>
2023-02-22 00:11:36 +0530

    chore(dev): adding stitches

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,5 +3,18 @@ import type { AppProps } from 'next/app'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-size: 16px;
+          color: var(--colors-black);
+          scroll-behavior: smooth;
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale;
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,16 @@
 import { Head, Html, Main, NextScript } from 'next/document'
 
+import { getCssText } from '~/stitches.config'
+
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head />
+      <Head>
+        <style
+          id='stitches'
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
