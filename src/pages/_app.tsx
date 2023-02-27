e97e8ed38c1c3269b270e1a6cd69c49d8bb688f0
Karthik Kamalakannan <karthik@hellonext.co>
2023-02-27 22:32:47 +0530

    chore(dev): upgrading next

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
 import type { AppProps } from 'next/app'
-
+import { Inter } from 'next/font/google'
 import '@/styles/globals.css'
+
+const inter = Inter({
+  subsets: ['latin']
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <style jsx global>{`
         html {
+          font-family: ${inter.style.fontFamily};
           font-size: 16px;
           color: var(--colors-black);
           scroll-behavior: smooth;
